36ab464ef02e9402c63eab15aee76fd8646ce2e6
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-19 01:39:16 +0330

    Style : Hero  & style Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Navbar from "./components/common/Navbar";
+import Hero from "./components/sections/Hero";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      {/* <Navbar /> */}
+      <Hero />
     </div>
   );
 }
