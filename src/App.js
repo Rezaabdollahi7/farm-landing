ca0ea76d72af4ebb84c268b7743c5300df81e40b
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-19 15:34:59 +0330

    Feat : Testimonials section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import Navbar from "./components/common/Navbar";
 import Hero from "./components/sections/Hero";
 import Features from "./components/sections/Features";
+import Testimonials from "./components/sections/Testimonials";
+
 function App() {
   return (
     <div className="App">
       {/* <Navbar /> */}
       <Hero />
       <Features />
+      <Testimonials />
     </div>
   );
 }
