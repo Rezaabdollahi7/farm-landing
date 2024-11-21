9c7dfc56697664f2c5bb513c2687a67c23d3f58e
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2024-11-21 22:56:00 +0330

    Update : chnage website logo

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import { ChevronRightIcon } from '@heroicons/react/20/solid'
-import Logo from '../../assets/icons/farm.png'
+import Logo from '../../assets/icons/farm1.png'
 
 const navigation = [
     { name: 'Home', href: '#', current: true },
