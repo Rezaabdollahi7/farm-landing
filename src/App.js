a6347c42235fb43c1afd670f7bae7ee798b8ab5f
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2025-01-30 00:36:33 +0330

    refactor: remove comments and image gallery sections

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Hero from "./components/sections/Hero";
 import Features from "./components/sections/Features";
 import WhatYouGet from "./components/sections/WhatYouGet";
-import Testimonials from "./components/sections/Testimonials";
+// import Testimonials from "./components/sections/Testimonials";
 import Contact from "./components/sections/Contact";
 import WebAppOverview from "./components/sections/WebAppoverview";
 import Footer from "./components/common/Footer";
 import HowItWorks from "./components/sections/HowItWorks";
-import Gallery from "./components/sections/Gallery";
+// import Gallery from "./components/sections/Gallery";
 import TechnologyOverview from "./components/sections/TechnologyOverview";
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
       <WebAppOverview />
       <TechnologyOverview />
       <Features />
-      <Testimonials />
-      <Gallery />
+      {/* <Testimonials />
+      <Gallery /> */}
       <Contact />
       <Footer />
     </div>
